460cd78c5e05d0554da76be1f72c1c977b7f82ef
variphx <variphx@gmail.com>
2025-05-07 11:13:29 +0700

    fix (cv_parser): fix tags to be html compliant

--- cv_parser/__init__.py
+++ cv_parser/__init__.py
@@ -6,17 +6,17 @@ from ..cvs import CurriculumVitae
 class CVParser:
     def __init__(self):
         self._prompt = """
-<|SYSYEM|>
+<system>
 You are a skillful headhunter who reads CVs and extracts relevant information of the candidate.
 Your task is to analyze the candidate's CV and extract their skills, experience, education, contact information and further information should they have been mentioned.
-<|SYSTEM|>
-<|SPECIFICATION|>
+</system>
+<specification>
 Note that the date time format is DD/MM/YYYY in the CVs, and the date or month may be omitted. The dash (-) between dates indicates a range of time.
 Examples:
     - 2018 - 2020 means start at 2018 and end at 2020.
     - 02/2025 means February 2025.
     - 10/2022 - 02/2023 means October 2022 to February 2023.
-<|SPECIFICATION|>
+</specification>
 Extract the information of the candidate in the following CV, be concise and precise.
 """
         self._client = genai.Client()
